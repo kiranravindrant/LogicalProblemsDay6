a419bc7ef536fa3afc74e567b6d9f0ca2e6253ae
kiranravindrant <kiranravindrant@gmail.com>
2021-07-12 23:19:03 +0530

    [kiran]Added:Program to list Fibonacci series

---
+++ Fibo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Fibonacci_Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value1 = 0, value2 = 1, value3, i, n;
+
+
+            Console.WriteLine("PROGRAM TO PRINT FIRST N FIBONACCI SERIES ");
+
+            Console.WriteLine("Enter value of N ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(value1);
+            Console.Write(" "+value2);
+
+            for (i = 2; i < n; i++)
+            {
+                value3 = value1 + value2;
+                Console.Write(" "+value3);
+                value1 = value2;
+                value2 = value3;
+
+
+            }
+        }
+
+    }
+}
+
+

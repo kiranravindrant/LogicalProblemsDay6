e0ac83aeb7f435241bbd7f3d6886191b2e90845d
kiranravindrant <kiranravindrant@gmail.com>
2021-07-15 00:13:23 +0530

    [kiran]Added:Program to Reverse a number added

---
+++ ReverseNo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num;
+            int reverse = 0;
+            Console.WriteLine("Enter the Number:  ");
+            Num = int.Parse(Console.ReadLine());
+
+
+
+            
+
+            while (Num > 0)
+            {
+                int digit = (Num % 10);
+                reverse = reverse * 10 + digit;
+                Num = (Num / 10);
+
+
+            }
+            Console.WriteLine(+reverse + ":is the reverse of the number");
+            Console.ReadLine();
+
+
+        }
+    }
+}

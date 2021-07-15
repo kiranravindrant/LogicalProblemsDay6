e3ccaed1e76b8bf2d2a9e54a89c7adf492817cbc
kiranravindrant <kiranravindrant@gmail.com>
2021-07-15 23:12:39 +0530

    [kiran]Added:main function_CouponNumberProgram

---
+++ Program_coupon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DistinctCouponNumbers
+{
+    class DistinctCouponNumbers
+    {
+        static void Main(string[] args)
+        {
+            DistinctRandom.CouponNumbers();
+            
+
+          
+        }
+    
+    }
+}
+
+                
+
+              
+
+
+            
+
+
+
+        
+   
+

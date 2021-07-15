016002753c4e2a58fc7b43edc93fae4668d9acff
kiranravindrant <kiranravindrant@gmail.com>
2021-07-15 16:16:58 +0530

    [kiran]Added: Program Stopwatch

---
+++ StopWatch.cs
@@ -0,0 +1,27 @@
+﻿
+	using System;
+	using System.Diagnostics;
+	using System.Threading.Tasks;
+
+namespace StopWatch
+{ 
+	class StopwatchTimer
+	{
+		static void Main()
+		{
+			Console.WriteLine("Press Enter to START THE TIMER");
+			Stopwatch timer = new Stopwatch();
+			Console.ReadLine();
+
+			timer.Start();
+
+			Console.WriteLine("TIMER RUNNING!!,  Press Enter to STOP");
+			Console.ReadLine();
+			timer.Stop();
+
+
+
+			Console.WriteLine("Time elapsed: " + timer.Elapsed);
+		}
+	}
+}
